dda1fb40998ade7f1ae44e65d263c25ad2a5369f
xuyin <axuyin@gmail.com>
2021-01-23 14:25:59 +0800

    beat1.0

---
+++ Unity/Assets/Baum2/Sample/Sample.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Baum2.Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject uiPrefab = null;
+
+        private System.Collections.Generic.List<string> listElements = new System.Collections.Generic.List<string>();
+
+        public void Start()
+        {
+
+            for (var i = 0; i < 1000; ++i)
+            {
+                listElements.Add(Random.Range(0, 2) + "_" + i);
+            }
+            ImageSample();
+            ButtonSample();
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void ImageSample()
+        {
+
+        }
+
+        public void ButtonSample()
+        {
+
+        }
+
+
+    }
+}
